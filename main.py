d9853929a1807c486350ea9840635a8d1c068810
Geraldine Guzman <s1095811@studenti.univpm.it>
2021-07-09 18:06:33 +0200

    Modifica Home e Lista Dipendenti

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import sys #Modulo che fornisce l'accesso ad alcune variabili , in questo caso sys.exit
 from PyQt5.QtWidgets import QApplication
 
-from home.views.VistaHome import VistaHome  #importa dalla directory home "VistaHome"
+from home.views.GUI_Home import Home  #importa dalla directory home "VistaHome"
 
 if __name__ == '__main__':
 
     #istanziamento per iniziare un'applicazione
     app = QApplication(sys.argv)
     #creo un oggetto della classe importata
-    vista_home = VistaHome()
+    vista_home = Home()
     # serve a mostrare a schermo la finestra vista home
     vista_home.show()
     #esegue l'applicazione
