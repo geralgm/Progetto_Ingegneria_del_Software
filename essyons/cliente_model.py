35eeb407d0ba44f272ebe3b2d11b3a4c59b37ad9
Geraldine Guzman <s1095811@studenti.univpm.it>
2021-07-09 18:15:03 +0200

    inserimento "Nuovo dipendente" non finito e immagini

--- essyons/cliente_model.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtCore import QDate
-
-
-class cliente_model():
-    def __init__(self, nome, telefono):
-        self.nome = nome
-        self.telefono = telefono
-        self.data = QDate.currentDate().toString("dddd d MMMM yyyy")
