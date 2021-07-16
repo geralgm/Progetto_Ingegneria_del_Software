b0acc821058cc9c12bed8d51de4860e7818fb8ba
Geraldine Guzman <s1095811@studenti.univpm.it>
2021-07-16 09:59:05 +0200

    Modifica VistaListaPrenotazioni

--- home/views/GUI_Home.py
+++ home/views/GUI_Home.py
@@ -10,7 +10,7 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from listadipendenti.views.GUI_ListaDipendenti import VistaListaDipendenti
-from listaprenotazione.views.vistaprenotazione import VistaListaPrenotazioni
+from listaprenotazioni.views.VistaListaPrenotazioni import VistaListaPrenotazioni
 from listaordini.views.VistaListaOrdine import VistaListaOrdini
 from PyQt5.QtWidgets import QWidget
 import images
